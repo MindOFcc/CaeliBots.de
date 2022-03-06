61f316ff885e0e16f15e27711cfb0e1d533475e7
MindOFcc <86679074+MindOFcc@users.noreply.github.com>
2022-03-06 15:20:29 +0100

    CaeliBots.de

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "verification": "",
+    "description": "Das ist ein Test",
+    "domain": "http://91.218.66.159:5006", // This Domain need to be without https:// or http:// (eg: hydrabot.fun not https://hydrabot.fun)
+    "google_analitics": "", 
+    "token": "OTQ5NzQ5MDE5NzE3MDA5NTEx.YiO4sw.gr8L7fn3_b6YE-MmzMFmb7kP-vI",
+    "https":"https://",
+    "port":"5006",
+    "client_id":"949749019717009511",
+    "secret":"bzQjUGj640dvh3-5vqudJDAPRtVOL1KZ",
+    "support_server_link":"https://discord.gg/RMnA4yUkHa",
+    "bot_name":"TestBot"
+   }
